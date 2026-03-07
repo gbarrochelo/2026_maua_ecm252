5ccfe24fcb1c2346334ff9cadd43ade9858b9dd7
Gabriel Barrochelo <gbarrochelo@gmail.com>
2026-03-07 10:40:35 -0300

    learn(ap03-react): encerra a apostila

---
+++ ap003_react_jsx_componentes/ap003/src/App.jsx
@@ -0,0 +1,30 @@
+import './styles.css'
+const App = () => {
+
+  
+
+  const textoDoRotulo = 'Nome:'
+
+  const obterTextoDoBotao = () => 'Enviar'
+
+  const aoClicar = () => alert('Clicou')
+
+  return <div style={{margin: 'auto', width: 576, backgroundColor: '#EEE', padding: 12, borderRadius: 8}}>
+    <label className= "rotulo" htmlFor="campoNome" style={{display: 'block', marginBottom: 8}}>
+      {textoDoRotulo}
+    </label>
+
+  {/*input#campoNome */}
+    <input type="text" id="campoNome" style={{paddingTop: 8, paddingBottom: 8, width: '100%', borderStyle: 'hidden', outiline:'none', borderRadius: 8}}/>
+
+  {/*buttoon{Enviar} */}
+    <button 
+    onClick{()=>aoClicar()}
+    style={estilosBotao}>
+     {obterTextoDoBotao}
+    </button>
+
+  </div>
+}
+
+export default App
